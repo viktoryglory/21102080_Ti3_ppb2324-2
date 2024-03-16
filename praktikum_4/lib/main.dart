48cedaaa44d5888ef2e12a30df9d124e0b8fe29d
FERI YASIN <21102080@ittelkom-pwt.ac.id>
2024-03-16 16:16:56 +0700

    Membuat column pertama pada listview

--- praktikum_4/lib/main.dart
+++ praktikum_4/lib/main.dart
@@ -13,8 +13,60 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Praktikum 4',
         home: Scaffold(
-          appBar: AppBar(),
-          body: ListView(),
+          appBar: AppBar(
+            leading: Icon(Icons.arrow_back),
+            backgroundColor: Colors.red,
+          ),
+          body: ListView(
+            children: [
+              Container(
+                height: 250,
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.lightBlueAccent),
+                      child: Icon(
+                        Icons.person,
+                        size: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    //Nama
+                    Text(
+                      "Feri Yasin",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "21102080",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Mahasiswa",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
