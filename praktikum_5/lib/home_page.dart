b8704c1ebfcb993b5a69dc7ce23634aa773aa0ea
FERI YASIN <21102080@ittelkom-pwt.ac.id>
2024-03-22 08:01:48 +0700

    menambahkan isi parameter pada class Myapp serta menambahkan tombol pada halaman body Homepage

--- praktikum_5/lib/home_page.dart
+++ praktikum_5/lib/home_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Menuju halaman kedua'),
+        ),
       ),
     );
   }
--- praktikum_5/lib/main.dart
+++ praktikum_5/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praktikum_5/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
